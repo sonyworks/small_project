28847caf2a00cbecc12698f19e690c0eb8835a3d
Sony <madhucheviti51@gmail.com>
2026-05-28 10:53:25 +0530

    add email sending

--- mailapp/views.py
+++ mailapp/views.py
@@ -1,11 +1,31 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
+from django.core.mail import send_mail
+from django.conf import settings
+
 class SendMailView(APIView):
+
 
  def get(self, request):
 
+    send_mail(
+        subject='Test Mail',
+
+        message='Hi\nHello\nHow are you',
+
+        from_email=settings.EMAIL_HOST_USER,
+
+        recipient_list=[
+            'sonycheviti@gmail.com'
+        ],
+
+        fail_silently=False,
+    )
+
     return Response({
-        "message": "Working"
+        "status": "success",
+        "message": "Mail sent successfully"
     })
+
 
