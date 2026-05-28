988958a60b80c193830355edd3926560cbe44d0d
Sony <madhucheviti51@gmail.com>
2026-05-28 10:17:49 +0530

    test api

--- mailapp/views.py
+++ mailapp/views.py
@@ -1,21 +1,13 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from django.core.mail import send_mail
-from django.conf import settings
-
 
 class SendMailView(APIView):
 
-    def get(self, request):
+```
+def get(self, request):
 
-        send_mail(
-            subject="Auto Mail",
-            message="Hi Hello How are you",
-            from_email=settings.EMAIL_HOST_USER,
-            recipient_list=["sonycheviti@gmail.com"],
-        )
-
-        return Response({
-            "status": "success",
-            "message": "Mail sent successfully"
-        })
+    return Response({
+        "status": "success",
+        "message": "API Working Successfully"
+    })
+```
